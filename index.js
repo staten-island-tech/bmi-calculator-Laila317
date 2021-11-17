05ebc021ee1c4b507b06575007e1165794656ec4
Laila317 <90777718+Laila317@users.noreply.github.com>
2021-11-16 20:59:54 -0500

    DONE!

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const user = {
+    firstName: "Patrick",
+    lastName: "Star",
+    Bodytype: "Not an hourglass, but a star"
+    
+    };
 
 function bmi(){
 
-  
 
     let h = document.getElementById('height').value;
 
@@ -11,34 +16,33 @@ function bmi(){
 
     let bmi = w/ (h/100 *h/100);
 
-
-
     let total = bmi.toFixed(2);
 
 
 
 document.getElementById('result').innerHTML = "Your BMI is " + total;
 
+let range = '';
+if (bmi < 18.5){
+    range = "Underweight";
+}
+
+if (bmi >= 18.5 && bmi < 25 ) {
+range = "Normal";
 
 }
 
-const user = {
-firstName: "Patrick",
-lastName: "Star",
-Bodytype: "Not an hourglass, but a star"
+if (bmi >= 25 && bmi < 30){
+    range = "Overweight";
+
+}
+
+if (bmi >= 30){
+    range = "Obese";
+}
+
+document.getElementById('comment').innerHTML = `Range: <span id= "comment"> ${range} </span>`;
+
+
 
 };
-
-
-
-
-
-
-
- 
- 
-
-
-
-
-
