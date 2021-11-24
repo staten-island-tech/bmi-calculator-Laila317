c305282ba02635b7fa581bcb324f29c712848681
Laila317 <90777718+Laila317@users.noreply.github.com>
2021-11-24 09:42:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const user = {
+const person = {
     firstName: "Patrick",
     lastName: "Star",
     Weight: "78kg",
@@ -7,19 +7,10 @@ const user = {
     };
 
 function bmi(){
-
-
     let h = document.getElementById('height').value;
-
-
     let w = document.getElementById('weight').value;
-
-
     let bmi = w/ (h/100 *h/100);
-
     let total = bmi.toFixed(2);
-
-
 
 document.getElementById('result').innerHTML = "Your BMI is " + total;
 
@@ -30,20 +21,21 @@ if (bmi < 18.5){
 
 if (bmi >= 18.5 && bmi < 25 ) {
 range = "Normal";
-
 }
 
 if (bmi >= 25 && bmi < 30){
     range = "Overweight";
-
 }
-
 if (bmi >= 30){
-    range = "Obese";
+        range = "Obese";
 }
 
 document.getElementById('comment').innerHTML = `Range: <span id= "comment"> ${range} </span>`;
 
-
-
 };
+
+function argument(person){
+    console.log(`this is ${person}`);
+}
+
+argument("Patrick Star,  Weight: 78kg, Height:110cm ");
